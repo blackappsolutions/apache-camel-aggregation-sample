4dcc4c3e6b39755eaf32778c773195af8750c514
Markus Schwarz <markus.schwarz@payback.net>
2018-11-27 14:33:18 +0100

    Skip selected Exchanges

--- src/main/java/test/JpaRouteBuilder.java
+++ src/main/java/test/JpaRouteBuilder.java
@@ -1,12 +1,18 @@
 package test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
 import org.apache.camel.Endpoint;
 import org.apache.camel.Exchange;
 import org.apache.camel.LoggingLevel;
+import org.apache.camel.Message;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.component.jpa.DeleteHandler;
 import org.apache.camel.component.jpa.JpaComponent;
+import org.apache.camel.component.jpa.JpaConstants;
 import org.apache.camel.component.jpa.JpaEndpoint;
 import org.apache.camel.impl.DefaultCamelContext;
 import org.apache.camel.processor.aggregate.AbstractListAggregationStrategy;
@@ -14,10 +20,8 @@ import org.apache.camel.processor.aggregate.AggregationStrategy;
 import org.apache.camel.support.ExpressionAdapter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import test.data.Master;
 
-import javax.persistence.Query;
-import java.util.ArrayList;
+import test.data.Master;
 
 import static java.lang.System.out;
 
@@ -26,39 +30,52 @@ import static java.lang.System.out;
  */
 public class JpaRouteBuilder extends RouteBuilder {
     private static final Logger LOGGER = LoggerFactory.getLogger(JpaRouteBuilder.class);
+    private static final String DO_NOT_DELETE = "doNotDelete";
 
     public void configure() throws Exception {
         from(createProducer("jpa:test.data.Master"))
-            .aggregate(correlationExpression, groupedBodyAggregationStrategy)
-            .completionFromBatchConsumer()
-            .log(LoggingLevel.DEBUG, LOGGER, "trx_imp_notification")
-            .process(processor)
-            .to(createConsumer())
-            .end();
+                .aggregate(correlationExpression, groupedBodyAggregationStrategy)
+                .completionFromBatchConsumer()
+                .log(LoggingLevel.DEBUG, LOGGER, "trx_imp_notification")
+                .process(processor)
+                .choice()
+                .when(header(DO_NOT_DELETE).isEqualTo(true))
+                .stop()
+                .otherwise()
+                .process(deletionHandler)
+                .to(createConsumer())
+                .end();
     }
 
-    String createConsumer() {
+    private String createConsumer() {
         return "file:target";
     }
 
-    Endpoint createProducer(String uri) throws Exception {
+    private Endpoint createProducer(String uri) throws Exception {
         JpaComponent jpaComponent = new JpaComponent();
         jpaComponent.setCamelContext(new DefaultCamelContext());
         JpaEndpoint endpoint = (JpaEndpoint) jpaComponent.createEndpoint(uri);
-        endpoint.setDeleteHandler(deletionHandler);
         endpoint.setNamedQuery(Master.SELECT_ALL);
         endpoint.setDelay(60000);
         endpoint.setTransacted(true);
+        endpoint.setConsumeDelete(false);
         return endpoint;
     }
 
-    DeleteHandler deletionHandler = (entityManager, entityBean, exchange) -> {
-        Master master = (Master) entityBean;
-        Query detailQuery = entityManager.createNativeQuery("DELETE FROM DETAIL WHERE stepNumber=" + master.masterID.stepNumber + " AND stepName='" + master.masterID.stepName + "'");
-        detailQuery.executeUpdate();
-        Query masterQuery = entityManager.createNativeQuery("DELETE FROM MASTER WHERE stepNumber=" + master.masterID.stepNumber + " AND stepName='" + master.masterID.stepName + "'");
-        masterQuery.executeUpdate();
+    private Processor deletionHandler = exchange -> {
+        Message in = exchange.getIn();
+        EntityManager entityManager = in.getHeader(JpaConstants.ENTITY_MANAGER, EntityManager.class);
+        List<Master> masterList = (List<Master>) in.getBody();
+        for (Master master : masterList) {
+            entityManager.createNativeQuery(createDeleteStmt(master, "DETAIL")).executeUpdate();
+            entityManager.createNativeQuery(createDeleteStmt(master, "MASTER")).executeUpdate();
+        }
     };
+
+    private String createDeleteStmt(Master master, String tableName) {
+        return "DELETE FROM " + tableName + " WHERE stepNumber=" + master.masterID.stepNumber + " AND stepName='" +
+                master.masterID.stepName + "'";
+    }
 
     ExpressionAdapter correlationExpression = new ExpressionAdapter() {
         @Override
@@ -78,7 +95,10 @@ public class JpaRouteBuilder extends RouteBuilder {
         }
     };
 
+    boolean b = false;
+
     Processor processor = exchange -> {
+
         ArrayList list = exchange.getIn().getBody(ArrayList.class);
         out.println("-------------------------------------");
         for (Object listItem : list) {
@@ -86,7 +106,14 @@ public class JpaRouteBuilder extends RouteBuilder {
             out.println(entity.masterID.stepName + " | " + entity.groupId);
         }
         out.println("-------------------------------------");
-    };
 
+        if (b) {
+            exchange.getIn().setHeader(DO_NOT_DELETE, true);
+        }
+
+        if (!b) {
+            b = true;
+        }
+    };
 
 }
